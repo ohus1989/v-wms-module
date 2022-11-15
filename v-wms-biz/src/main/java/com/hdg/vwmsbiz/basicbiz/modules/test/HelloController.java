6114fa776a2dd4527f9414df7ab111720178110d
오의섭 <ohus1989@naver.com>
2022-11-16 00:50:06 +0900

    gateway 설정중

--- v-wms-biz/src/main/java/com/hdg/vwmsbiz/basicbiz/modules/test/HelloController.java
+++ v-wms-biz/src/main/java/com/hdg/vwmsbiz/basicbiz/modules/test/HelloController.java
@@ -1,11 +1,13 @@
 package com.hdg.vwmsbiz.basicbiz.modules.test;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/biz")
 public class HelloController {
-    @GetMapping("/")
+    @GetMapping("/test1")
     public String index() {
         return "Greetings from Spring Boot!";
     }
